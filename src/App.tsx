0193023a7f3f36c03be00fa19d87020ee1600666
publdaze <publdaze@naver.com>
2022-06-30 16:34:00 +0900

    feat) 라우터 설정
    - 라우터 설정
    - Todo, SignIn 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Todo = lazy(() => import("./page/Todo/Todo"));
+const SignIn = lazy(() => import("./page/SignIn/SignIn"));
 
 function App() {
-  return <div className="App">init</div>;
+  return (
+    <div className="App">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Todo />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </Router>
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
