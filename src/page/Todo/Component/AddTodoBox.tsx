8eaba488b9c61f75a297522cb14f9dcd91e99be2
publdaze <publdaze@naver.com>
2022-07-03 16:24:11 +0900

    feat) todo 추가하기 기능 구현
    - submit 후 로컬스토리지 저장
    - 로컬스토리지에 저장되 이전값 가져와서 추가한 값 업데이트

--- src/page/Todo/Component/AddTodoBox.tsx
+++ src/page/Todo/Component/AddTodoBox.tsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState, ChangeEventHandler, FormEventHandler } from "react";
 import { PlusIcon } from "@heroicons/react/outline";
 
 const AddTodoBox = () => {
+  const [todo, setTodo] = useState("");
+  const prevTodoList = JSON.parse(localStorage.getItem("todoList") || "[]");
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+
+    localStorage.setItem("todoList", JSON.stringify([...prevTodoList, todo]));
+    setTodo(""); // input창 비우기
+  };
+
   return (
-    <div className="mt-16 px-5 w-full h-16 flex items-center bg-blue-50 border rounded-md">
-      <PlusIcon className="mr-5 w-7 h-7 border rounded-md border-blue-100 bg-blue-300 text-white"></PlusIcon>
+    <form
+      className="mt-16 px-5 w-full h-16 flex items-center bg-blue-50 border rounded-md"
+      onSubmit={handleSubmit}
+    >
+      <button
+        type="submit"
+        className="mr-5 w-7 h-7 border rounded-md border-blue-100 bg-blue-300 text-white"
+      >
+        <PlusIcon />
+      </button>
       <input
         className="w-full h-9 bg-transparent border-b border-blue-300 focus:outline-none placeholder:text-blue-300"
         placeholder="Todo를 입력해주세요."
-      ></input>
-    </div>
+        onChange={handleInputChange}
+        value={todo}
+      />
+    </form>
   );
 };
 
